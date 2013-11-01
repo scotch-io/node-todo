2d9f2a98dfa46370fd9b0f2addbe7238df3cc576
Chris Sevilleja <chris@scotch.io>
2013-11-01 11:32:02 -0700

    adding routes

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ var server   = require('http').createServer(app); 		// setup http server
 var io       = require('socket.io').listen(server); 	// socket.io
 var mongoose = require('mongoose'); 					// mongoose for mongodb
 
+	// routes
+	var routes = require('./routes/app');
+	var api    = require('./routes/api');
+
 // configuration ===============================================================
+
+mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu');
+
+app.configure(function() {
 
 	// views
 	app.set('views', __dirname + '/views'); 				// set location for views
@@ -14,18 +22,22 @@ var mongoose = require('mongoose'); 					// mongoose for mongodb
 	// express modules
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 	app.use(express.logger('dev')); 						// log every request to the console
-	app.use(express.cookieParser()); 						// read cookies
 	app.use(express.bodyParser()); 							// pull information from html in POST
 	app.use(express.methodOverride()); 						// simulate DELETE and PUT
 
+});
+
 // routes ======================================================================
 
+	// application
+	app.get('*', app.index);
 
-	// main routes
-	app.get('/', function(req, res) {
-		res.sendfile('views/index.html');
-	});
-
+	// api
+	app.get('/api/todos', api.all); 					// get all todos
+	app.get('/api/todos/:todo_id', api.show); 			// get single todo
+	app.post('/api/todos', api.create); 				// create todo
+	app.put('/api/todos/:todo_id', api.edit); 			// edit todo
+	app.delete('/api/todos/:todo_id', api.delete);    	// delete
 
 // listen ======================================================================
 server.listen(8080, function()

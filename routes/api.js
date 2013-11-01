4bfc1d7d6a52edacc81365f9b5f6b2cb3bba37cd
Chris Sevilleja <chris@scotch.io>
2013-11-01 11:55:37 -0700

    working on templating

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+/**
+	All Todos
+*/
+exports.index = function(req, res) {
+	console.log('fuck');
+};
+
+/**
+	Get a single todo
+*/
+exports.show = function(req, res) {
+	console.log('fuck');
+}
+
+/**
+	Create todo
+*/
+exports.create = function(req, res) {
+	console.log('fuck');
+}
+
+/**
+	Edit todo
+*/
+exports.edit = function(req, res) {
+	console.log('fuck');
+}
+
+/**
+	Delete a todo
+*/
+exports.delete = function(req, res) {
+	console.log('fuck');
+}
--- server.js
+++ server.js
@@ -13,34 +13,30 @@ var mongoose = require('mongoose'); 					// mongoose for mongodb
 
 mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu');
 
-app.configure(function() {
+// views
+app.set('views', __dirname + 'app/views'); 				// set location for views
+app.set('view engine', 'jade');
 
-	// views
-	app.set('views', __dirname + '/views'); 				// set location for views
-	app.set('view engine', 'html');
-
-	// express modules
-	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-	app.use(express.logger('dev')); 						// log every request to the console
-	app.use(express.bodyParser()); 							// pull information from html in POST
-	app.use(express.methodOverride()); 						// simulate DELETE and PUT
-
-});
+// express modules
+app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(express.logger('dev')); 						// log every request to the console
+app.use(express.bodyParser()); 							// pull information from html in POST
+app.use(express.methodOverride()); 						// simulate DELETE and PUT
 
 // routes ======================================================================
 
-	// application
-	app.get('*', app.index);
-
 	// api
-	app.get('/api/todos', api.all); 					// get all todos
+	app.get('/api/todos', api.index); 					// get all todos
 	app.get('/api/todos/:todo_id', api.show); 			// get single todo
 	app.post('/api/todos', api.create); 				// create todo
 	app.put('/api/todos/:todo_id', api.edit); 			// edit todo
 	app.delete('/api/todos/:todo_id', api.delete);    	// delete
 
+	// application
+	app.get('*', routes.index);
+
 // listen ======================================================================
 server.listen(8080, function()
 {
-	console.log('App started on port 8080');
+	console.log("App listening on port 8080");
 });

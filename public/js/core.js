7ccdca9973a51422256769dcd65cd8fe030243e5
Chris Sevilleja <chris@scotch.io>
2013-11-01 12:54:38 -0700

    angular start

---
+++ public/js/core.js
@@ -0,0 +1,28 @@
+var todo = angular.module('todo', [])
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl 	: 'todos.html',
+				controller 		: 'todos'
+			})
+			.when('/', {
+				templateUrl 	: 'todos.html',
+				controller 		: 'todos'
+			})
+			.when('/', {
+				templateUrl 	: 'todos.html',
+				controller 		: 'todos'
+			})
+			.when('/', {
+				templateUrl 	: 'todos.html',
+				controller 		: 'todos'
+			});
+		$locationProvider.html5Mode(true);
+	}])
+	.factory('Todo', function($resource) {
+		return {
+			getAllTodos : function() {
+				return $resource('/api/todos').query();
+			}
+		}
+	});

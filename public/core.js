803842ad0721e8f2429bb1d6e63f8d8aca1112fa
Chris Sevilleja <chris@scotch.io>
2013-11-13 08:45:27 -0800

    removing ng-init

--- public/core.js
+++ public/core.js
@@ -4,15 +4,13 @@ function mainController($scope, $http) {
 	$scope.formData = {};
 
 	// when landing on the page, get all todos and show them
-	$scope.initialize = function() {
-		$http.get('/api/todos')
-			.success(function(data) {
-				$scope.todos = data;
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
-	};
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
 
 	// when submitting the add form, send the text to the node API
 	$scope.createTodo = function() {

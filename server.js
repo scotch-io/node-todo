7051feb0255734eea73beb2e87931659d6364b09
Chris Sevilleja <chris@scotch.io>
2014-07-18 09:36:17 -0700

    setting morgan for dev

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var methodOverride = require('method-override');
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan()); // log every request to the console
+app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json

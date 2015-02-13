68fa11fe6b6160ce8da260edf30d3e6f5486781f
Garvit Sharma <garvit.sharma@qliktag.com>
2015-02-13 17:16:12 +0530

    Edit feature added

--- app/routes.js
+++ app/routes.js
@@ -1,57 +1,76 @@
 var Todo = require('./models/todo');
 
 function getTodos(res){
-	Todo.find(function(err, todos) {
+  Todo.find(function(err, todos) {
 
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
-				res.send(err)
+      // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+      if (err)
+        res.send(err)
 
-			res.json(todos); // return all todos in JSON format
-		});
+      res.json(todos); // return all todos in JSON format
+    });
 };
 
 module.exports = function(app) {
 
-	// api ---------------------------------------------------------------------
-	// get all todos
-	app.get('/api/todos', function(req, res) {
+  // api ---------------------------------------------------------------------
+  // get all todos
+  app.get('/api/todos', function(req, res) {
 
-		// use mongoose to get all todos in the database
-		getTodos(res);
-	});
+    // use mongoose to get all todos in the database
+    getTodos(res);
+  });
 
-	// create todo and send back all todos after creation
-	app.post('/api/todos', function(req, res) {
+  // create todo and send back all todos after creation
+  app.post('/api/todos', function(req, res) {
 
-		// create a todo, information comes from AJAX request from Angular
-		Todo.create({
-			text : req.body.text,
-			done : false
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
+    // create a todo, information comes from AJAX request from Angular
+    Todo.create({
+      text : req.body.text,
+      done : false
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
 
-			// get and return all the todos after you create another
-			getTodos(res);
-		});
+      // get and return all the todos after you create another
+      getTodos(res);
+    });
 
-	});
+  });
 
-	// delete a todo
-	app.delete('/api/todos/:todo_id', function(req, res) {
-		Todo.remove({
-			_id : req.params.todo_id
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
+  app.put('/api/todos', function(req, res){
+    console.log(req.body);
+    var todoId = req.body.todoId
+      , todoText = req.body.todoText;
 
-			getTodos(res);
-		});
-	});
+    Todo.update({
+      _id:todoId
+    },
+    {
+      $set:{text:todoText}
+    },
+    function(err, todo){
+      if(err)
+        res.send(err);
+      res.send("Success");
+    }
+    );
+  });
 
-	// application -------------------------------------------------------------
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-	});
+  // delete a todo
+  app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+      _id : req.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      getTodos(res);
+    });
+  });
+
+  // application -------------------------------------------------------------
+  app.get('*', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+  });
 };
--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,50 +1,71 @@
 angular.module('todoController', [])
 
-	// inject the Todo service factory into our controller
-	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
-		$scope.formData = {};
-		$scope.loading = true;
+  // inject the Todo service factory into our controller
+  .controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
+    $scope.formData = {};
+    $scope.loading = true;
+    $scope.editable=[];
 
-		// GET =====================================================================
-		// when landing on the page, get all todos and show them
-		// use the service to get all the todos
-		Todos.get()
-			.success(function(data) {
-				$scope.todos = data;
-				$scope.loading = false;
-			});
+    // GET =====================================================================
+    // when landing on the page, get all todos and show them
+    // use the service to get all the todos
+    Todos.get()
+      .success(function(data) {
+        $scope.todos = data;
+        for(var iter=0; iter<data.length; iter++){
+          $scope.editable[iter]=false;
+        }
+        $scope.loading = false;
+      });
 
-		// CREATE ==================================================================
-		// when submitting the add form, send the text to the node API
-		$scope.createTodo = function() {
+    // CREATE ==================================================================
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
 
-			// validate the formData to make sure that something is there
-			// if form is empty, nothing will happen
-			if ($scope.formData.text != undefined) {
-				$scope.loading = true;
+      // validate the formData to make sure that something is there
+      // if form is empty, nothing will happen
+      if ($scope.formData.text != undefined) {
+        $scope.loading = true;
 
-				// call the create function from our service (returns a promise object)
-				Todos.create($scope.formData)
+        // call the create function from our service (returns a promise object)
+        Todos.create($scope.formData)
 
-					// if successful creation, call our get function to get all the new todos
-					.success(function(data) {
-						$scope.loading = false;
-						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.todos = data; // assign our new list of todos
-					});
-			}
-		};
+          // if successful creation, call our get function to get all the new todos
+          .success(function(data) {
+            $scope.loading = false;
+            $scope.formData = {}; // clear the form so our user is ready to enter another
+            $scope.todos = data; // assign our new list of todos
+          });
+      }
+    };
 
-		// DELETE ==================================================================
-		// delete a todo after checking it
-		$scope.deleteTodo = function(id) {
-			$scope.loading = true;
+    // DELETE ==================================================================
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+      $scope.loading = true;
 
-			Todos.delete(id)
-				// if successful creation, call our get function to get all the new todos
-				.success(function(data) {
-					$scope.loading = false;
-					$scope.todos = data; // assign our new list of todos
-				});
-		};
-	}]);
+      Todos.delete(id)
+        // if successful creation, call our get function to get all the new todos
+        .success(function(data) {
+          $scope.loading = false;
+          $scope.todos = data; // assign our new list of todos
+        });
+    };
+
+    $scope.makeEditable = function(id) {
+      if(!$scope.editable[id])
+        $scope.editable[id]=true;
+    };
+
+    $scope.saveEdited = function(id, todoId, todoText) {
+      var todoData={todoId:todoId, todoText:todoText};
+      $scope.loading=true;
+      Todos.edit(todoData)
+        .success(function(data){
+          $scope.loading=false;
+          if($scope.editable[id])
+            $scope.editable[id]=false;
+        });
+    };
+
+  }]);

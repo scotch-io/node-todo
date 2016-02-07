536a58a734d8cab484245e92f5dc3325aae85d17
Jason Rice <jsnrice@gmail.com>
2016-02-06 21:10:17 -0500

    Setup local Mongo instance config and remote config. Updated dependencies.

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-
-	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-}
+    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+};

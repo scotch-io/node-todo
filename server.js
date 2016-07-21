88c7975d507fea9f01ea517de6ab093dc36376b9
Chris Sevilleja <chris@scotch.io>
2016-07-21 10:32:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var methodOverride = require('method-override');
 // configuration ===============================================================
 mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json

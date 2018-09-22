0e70b72d464e9d6af08a596d67a56eaae96c085f
Md. Shafiqul Islam <uzzal2k5@gmail.com>
2018-09-22 12:08:05 +0600

    mongodb

--- mongodb/db.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-docker run --name node-mongo -v /my/custom:/etc/mongo -d mongo: --config /etc/mongo/mongod.conf
-
-mongo --host localhost:27017 -u nodeuser -p nodepassword --authenticationDatabase admin
-
-docker run -d --name some-mongo -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
-
-docker run -it --rm --link some-mongo:mongo mongo mongo --host mongo -u mongoadmin -p secret --authenticationDatabase admin some-db

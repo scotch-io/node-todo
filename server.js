3847ead3c6f20d18eb18e4e8de50186f3760f273
Aliaksei Ramanouski <manlyx10101@gmail.com>
2020-04-15 22:28:57 +0300

    Add linter

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 var express = require('express');
 var app = express(); 						// create our app w/ express
 var mongoose = require('mongoose'); 				// mongoose for mongodb
-var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var port = process.env.PORT || 4201; 				// set the port
+var database = require('./config/database');			// load the database config
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
